46768fadd8781576804cb66347b1abcbbfcac522
Gilmar Pupo <g@g1mr.com>
2015-06-29 17:17:27 -0300

    Block all ports

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #
+# This file is part of gpupo/kakashi
+# https://github.com/gpupo/kakashi
+#
+# (c) Gilmar Pupo <g@g1mr.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
 while read -r line
 do
     perl $1 "$line";
