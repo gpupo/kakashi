8de6172e3d3552eb9b19f5659a4b661a5f08e7b4
Gilmar Pupo <g@g1mr.com>
2015-08-17 12:02:03 -0300

    warning

--- src/00-header.pl
+++ src/00-header.pl
@@ -6,3 +6,5 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 #
+# Warning: This file is generated automatically.
+# To improve it, see bin/build.sh and edit the corresponding source code
