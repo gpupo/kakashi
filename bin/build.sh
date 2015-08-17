fb27ac2f5a129fb874172e6ba83a90d27b224780
Gilmar Pupo <g@g1mr.com>
2015-08-17 11:19:02 -0300

    sharingan

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 RELEASE_ID=$(date +build-%Y-%m-%d-%Hh%M);
 
+# Custom
 cat src/00-header.pl > regex.custom.pm;
 echo "# $RELEASE_ID" >> regex.custom.pm;
-cat src/00-init.pl src/mta/* src/http/* src/99-footer.pl >> regex.custom.pm;
+cat src/00-init.pl src/mta/* src/http/* >> regex.custom.pm;
+
+
+#Sharingan
+cat regex.custom.pm > regex.sharingan.pm;
+cat src/sharingan/* >> regex.sharingan.pm;
+
+cat src/99-footer.pl  >> regex.custom.pm;
+cat src/99-footer.pl  >> regex.sharingan.pm;
