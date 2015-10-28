0b7bd3c850fc12d3ef463e08466b5aaae2e0d29e
Gilmar Pupo <g@g1mr.com>
2015-10-28 15:57:39 -0200

    reverse whitelist

---
+++ data/ip/whitelist/reverse.sh
@@ -0,0 +1,12 @@
+# Reverse IPs
+
+csf -a  157.55.39.0/24  'Kakashi whitelist for reverse response msn.com ';
+csf -a  207.46.13.0/24  'Kakashi whitelist for reverse response msn.com ';
+csf -a  65.55.213.0/24  'Kakashi whitelist for reverse response msn.com ';
+csf -a  66.102.6.0/24  'Kakashi whitelist for reverse response google.com ';
+csf -a  66.249.69.0/24  'Kakashi whitelist for reverse response googlebot.com ';
+csf -a  66.249.75.0/24  'Kakashi whitelist for reverse response googlebot.com ';
+csf -a  66.249.83.0/24  'Kakashi whitelist for reverse response google.com ';
+csf -a  66.249.88.0/24  'Kakashi whitelist for reverse response google.com ';
+csf -a  66.249.89.0/24  'Kakashi whitelist for reverse response google.com ';
+csf -a  68.180.230.0/24  'Kakashi whitelist for reverse response yahoo.net ';
