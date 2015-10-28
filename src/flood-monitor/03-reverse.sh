844c33db57896cf9c96a211fa35d03e991e09f26
Gilmar Pupo <g@g1mr.com>
2015-10-28 15:57:18 -0200

    reverse

---
+++ src/flood-monitor/03-reverse.sh
@@ -0,0 +1,24 @@
+
+reverseDNSLookupDomain() {
+    host $1 | rev | cut -d "." -f2-3 | rev;
+};
+
+reverseMonitor() {
+    for L in `cat /tmp/kakashi-flood-result| tr -s " "| tr "\t" ";" | tr " " ";"`;do
+       IP=$(echo $L | cut -d ";" -f 3)
+       COUNT=$(echo $L | cut -d ";" -f 2)
+       reverseDomain=$(reverseDNSLookupDomain $IP);
+       listed=0;
+       if grep -q "$reverseDomain" ~/.kakashi/reverse.deny; then
+           listed=1;
+           if [ "$DEFAULT_ACTION" == "" ];then
+               choiceActionForIp $IP;
+           else
+               echo "Default action for [$reverseDomain]: $DEFAULT_ACTION";
+               actionForIp $IP $DEFAULT_ACTION
+           fi
+       fi
+
+       echo "reverse,$reverseDomain,$IP,$listed,$COUNT" >> /var/log/kakashi-flood-reverse-monitor.log ;
+    done
+}
