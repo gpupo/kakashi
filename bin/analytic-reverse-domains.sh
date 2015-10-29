10f1af3447f393273037ca88f09f07ca350e30a6
Gilmar Pupo <g@g1mr.com>
2015-10-29 16:44:46 -0200

    reverse functions

--- bin/analytic-reverse-domains.sh
+++ bin/analytic-reverse-domains.sh
@@ -1,3 +1,10 @@
 cat /var/log/kakashi-flood-reverse-monitor.log | grep "reverse" |\
 grep -v -f ~/.kakashi/reverse.allow | grep -v -f ~/.kakashi/reverse.deny | \
-cut -d "," -f 2 | sort -u  > ~/.kakashi/reverse.domains.txt
+cut -d "," -f 2 | sort -u  | tee ~/.kakashi/reverse.domains.txt
+
+#Reorder Deny List
+kakashi-reverse-reorder-list() {
+    cat ~/.kakashi/reverse.deny | sort -u > /tmp/kakashi-reverse-deny; cat /tmp/kakashi-reverse-deny  > ~/.kakashi/reverse.deny;
+}
+
+kakashi-reverse-deny() { echo "$1" >> ~/.kakashi/reverse.deny; }
