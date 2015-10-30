70735e6e532d9aac87177052ccdeb8d13a285b9e
Gilmar Pupo <g@g1mr.com>
2015-10-30 09:53:41 -0200

    suspect

--- bin/analytic-reverse-domains.sh
+++ bin/analytic-reverse-domains.sh
@@ -20,6 +20,10 @@ kakashi-reverse-allow() {
     kakashi-reverse-add-to-list $1 "allow";
 }
 
+kakashi-reverse-suspect() {
+    kakashi-reverse-add-to-list $1 "suspect";
+}
+
 domain-whois-info() {
     echo "=====$L=====";
     whois "$1" | grep Registr;
@@ -31,22 +35,24 @@ actionForDomain() {
     case "$ACTION" in
       d ) kakashi-reverse-deny $DOMAIN;;
       a ) kakashi-reverse-allow $DOMAIN;;
+      s ) kakashi-reverse-suspect $DOMAIN;;
       * ) echo "none";;
     esac
 }
 
 
 cat /var/log/kakashi-flood-reverse-monitor.log | grep "reverse" |\
-grep -v -f ~/.kakashi/reverse.allow | grep -v -f ~/.kakashi/reverse.deny | \
+grep -v -f ~/.kakashi/reverse.allow | grep -v -f ~/.kakashi/reverse.deny | grep -v -f ~/.kakashi/reverse.suspect | \
 cut -d "," -f 2 | sort -u  | tee ~/.kakashi/reverse.domains.txt
 
 for L in `cat ~/.kakashi/reverse.domains.txt`;do
    domain-whois-info $L;
-   echo "Deny = d | Add to flood whitelist = a | ENTER for do nothing"
-   read -p "Action for $L? (d/a): " choice
+   echo "Deny = d | Add to flood whitelist = a | Suspect = s | ENTER for do nothing"
+   read -p "Action for $L? (d/a/s): " choice
    actionForDomain $L $choice
 done
 
 
 kakashi-reverse-reorder-list deny
 kakashi-reverse-reorder-list allow
+kakashi-reverse-reorder-list suspect
