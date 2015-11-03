026eb63636af85815c7ed47a8d856762f39f01b6
Gilmar Pupo <g@g1mr.com>
2015-11-03 10:32:33 -0200

    common

--- bin/kill-fake-bot.sh
+++ bin/kill-fake-bot.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-reverseDNSLookupDomain() {
-    host $1 | tr "\n" " " | rev | cut -d "." -f2-3 | rev;
-}
+APP_PATH="$(dirname $0)";
+source $APP_PATH/common.sh;
 
 for IP in `grep "Googlebot\|bingbot\|msnbot\|adsbot" /var/log/httpd/access_log | cut -d " " -f1 | sort -u`;do
-   h=reverseDNSLookupDomain $IP
-   echo $h
+   h=$(host "$IP" | tr "\n" " " | rev | cut -d "." -f2-3 | rev)
+   if grep -q "$h" ~/kakashi/data/bots; then
+       echo -n "$executionId Ok bot [$h]";
+   else
+       echo -n "$executionId Fake bot [$h]";
+   fi
 done
