b52799bf1fc0db2099cd42efcf0e5c97c67ef48b
Gilmar Pupo <g@g1mr.com>
2015-11-03 10:52:32 -0200

    headers

--- bin/looping-script-default.sh
+++ bin/looping-script-default.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+#  This file is part of gpupo/kakashi
+# http://www.g1mr.com/kakashi
+#
+# (c) Gilmar Pupo <g@g1mr.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
 # Monitor and Blocking
 
 while true; do ~/kakashi/bin/flood-monitor.sh; sleep 300; done
