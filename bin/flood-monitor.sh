8aefd07545e437c9c0b17c6f4f36c4700f2ad934
Gilmar Pupo <g@g1mr.com>
2015-11-06 09:07:23 -0200

    build

--- bin/flood-monitor.sh
+++ bin/flood-monitor.sh
@@ -11,7 +11,7 @@
 # Warning: This file is generated automatically.
 # To improve it, see bin/build.sh and edit the corresponding source code
 #
-# build-2015-11-03-10h46 | source: src/flood-monitor/
+# build-2015-11-06-09h06 | source: src/flood-monitor/
 #
 ##
 
@@ -34,60 +34,17 @@
 
 APP_PATH="$(dirname $0)";
 source $APP_PATH/common.sh;
-csf() { /usr/sbin/csf "$@"; }
-
-floodGrep() {
-    echo "";
-    grep $1 /var/log/httpd/access_log | tail -n 30 | cut -c1-120 | uniq;
-    echo "";
-    choiceActionForIp $1;
+floodList() {
+    compileIgnoreList;
+    tail -n 20000 $HTTPD_LOG_PATH \
+    | grep -v -f /tmp/kakashi-flood-ignore | tail -n 10000 | cut -d' ' -f 1,12-14 \
+    | tr ' "' "\t" | tr "(" " " | sort | uniq -c | sort -gr| head -n $SAMPLE_SIZE > /tmp/kakashi-flood-result;
 }
 
 compileIgnoreList() {
     csf -t | cut -d " " -f 3 | grep -v ^$ > /tmp/kakashi-flood-ignore;
     grep -v "^$\|^#" /etc/csf/csf.allow | cut -d '.' -f1,2,3 | uniq >> /tmp/kakashi-flood-ignore;
     grep -v ^$ ~/.kakashi/allow >> /tmp/kakashi-flood-ignore;
-}
-
-floodAllow() {
-    echo $1 >> ~/.kakashi/allow;
-}
-
-floodDenyTemp() {
-    COMMENT=${2-"flooder"};
-    csf -td $1 $DENY_TTL "$COMMENT" | tr "\n" ";";
-    echo "";
-}
-
-floodDeny() {
-    csf -d $1 $DENY_TTL flooder;
-}
-
-floodList() {
-    compileIgnoreList;
-    tail -n 20000 /var/log/httpd/access_log \
-    | grep -v -f /tmp/kakashi-flood-ignore | tail -n 10000 | cut -d' ' -f 1,12-14 \
-    | tr ' "' "\t" | tr "(" " " | sort | uniq -c | sort -gr| head -n $SAMPLE_SIZE > /tmp/kakashi-flood-result;
-}
-
-choiceActionForIp() {
-    IP=$1;
-    echo "Temporary Deny = t | Deny = d | More Info = i | Add to flood whitelist = a | ENTER for do nothing"
-    read -p "Action for $IP? (t/d/i/a): " choice
-    actionForIp $IP $choice
-}
-
-actionForIp() {
-    IP=$1;
-    ACTION=${2:-};
-    COMMENT=${3:-};
-    case "$ACTION" in
-      t ) floodDenyTemp $IP "$COMMENT";;
-      d ) floodDeny $IP;;
-      i ) floodGrep $IP;;
-      a ) floodAllow $IP;;
-      * ) echo "none";;
-    esac
 }
 
 floodMonitor() {
