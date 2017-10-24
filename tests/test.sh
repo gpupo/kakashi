683f121eac2283c25dffca87545bcf467894603f
Gilmar Pupo <contact@gpupo.com>
2017-10-24 11:06:32 -0200

    url/email update

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This file is part of gpupo/kakashi
-# http://www.g1mr.com/kakashi
+# https://opensource.gpupo.com/kakashi
 #
-# (c) Gilmar Pupo <g@g1mr.com>
+# (c) Gilmar Pupo <contact@gpupo.com>
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
